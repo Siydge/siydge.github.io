7d78a1f539dd35a26b2bebc8c0d7f00e8bb0338a
Siydge <53163523+Siydge@users.noreply.github.com>
2021-07-14 09:59:54 +1000

    Added

--- scripts/hobo_zombies-recoil.lua
+++ scripts/hobo_zombies-recoil.lua
@@ -1,9 +1,12 @@
--- this one is currently modified for ZOMBIES RECOIL
+
+-- this one is currently modified for SCP-TASK-FORCE
 
 -- Coded by Lock the hobo that steals your trash
 local settings = {
 	['Aimbot'] = {
 		['enabled'] = true,
+		['max_distance'] = 200,
+		['closes_to_crosshair'] = true,
 		['offset'] = {
 			['x'] = 0,
 			['y'] = 0,
@@ -14,12 +17,12 @@ local settings = {
 		['fov_color'] = {255,255,255},
 
 
-		['smoothness'] = 1,
-		['sensitivity'] = 2,
+		['smoothness'] = 10,
+		['sensitivity'] = 5,
 
 		['target_dot'] = true, -- false > off | true > on
-		['target_dot_size'] = 3,
-		['target_dot_color'] = {100,100,255},
+		['target_dot_size'] = 2,
+		['target_dot_color'] = {255,100,100},
 	},
 
 	['Esp'] = {
@@ -29,9 +32,14 @@ local settings = {
 		['tracer_color'] = {100,100,255},
 
 		['name'] = true,
-		['name_custom_text'] = 'Zombie',
+		['name_custom_text'] = '',
 		['name_offset'] = {20,-7},
-		['name_color'] = {100,100,255},
+		['name_color'] = {255,255,255},
+
+		['distance'] = false, -- false > off | true > on
+		['distance_behind_text'] = ' distance',
+		['distance_offset'] = {20,5},
+		['distance_color'] = {100,100,255},
 
 		['head_dot'] = true, -- false > off | true > on
 		['head_dot_size'] = 1,
@@ -39,9 +47,9 @@ local settings = {
 	},
 
 	['Npc Path'] = { -- the path from game to the folder/model where the npc is located and you can make it select more then one model/folder
-		[1] = {'Workspace','ClientZambies'},
+		[1] = {'Workspace'},
 	}, 
-	['In Npc Path'] = {'Head'}, -- the path from the npc model to the target part
+	['In Npc Path'] = {}, -- the path from the npc model to the target part
 } 
 
 local v = dx9
@@ -96,21 +104,23 @@ end
 function createsetup(loc)
 
 	for a,b in next, v.GetChildren(loc) do 
-
-		local head = gtffc(b,settings['In Npc Path'])
+		local head = gtffc(b,{'HumanoidRootPart'})
 		if head then 
 			local headpos = v.GetPosition(head)
-			local headwts = v.WorldToScreen({headpos.x,headpos.y, headpos.z})
+			local playerpos = player.Position
+			local headwts = v.WorldToScreen({headpos.x,headpos.y+2, headpos.z})
 			local headwts2 = {headwts.x,headwts.y}
 			local distance2 = caldist(headwts,mouse,false)
+			local distance3 = caldist(headpos,playerpos,true)
 
-			if (headwts.x > 0 or headwts.y > 0) then
+			if (headwts.x > 0 or headwts.y > 0) and distance3 < settings['Aimbot']['max_distance'] and distance3 >= 3 then
 				if closest ~= nil then 
-					if closest.dis > distance2 and distance2 < settings['Aimbot']['fov_size']  then 
+					if com(closest.dis,closest.dis3) > com(distance2,distance3) and distance2 < settings['Aimbot']['fov_size']  then 
 						closest = {
 							pos = headpos,
 							pos2 = headwts,
 							dis = distance2,
+							dis3 = distance3,
 						}
 					end
 				else 
@@ -119,6 +129,7 @@ function createsetup(loc)
 							pos = headpos,
 							pos2 = headwts,
 							dis = distance2,
+							dis3 = distance3,
 						}
 					end
 				end
@@ -135,6 +146,12 @@ function createsetup(loc)
 							text = setesp['name_custom_text']
 						end 
 						v.DrawString({headwts.x+setesp['name_offset'][1],headwts.y+setesp['name_offset'][2]},setesp['name_color'],text)
+					end
+
+					if setesp['distance'] then
+						local text = math.floor(distance3)
+						text = text..setesp['distance_behind_text']
+						v.DrawString({headwts.x+setesp['distance_offset'][1],headwts.y+setesp['distance_offset'][2]},setesp['distance_color'],text)
 					end
 
 					if setesp['tracer'] then  -- "v.size().width / 2, v.size().height" is from https://cultofintellect.com/dx9ware/docs/lua/examples.html, didn't made it 
